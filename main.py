a8504e8c12c2f0e5c6b085631c96b4908e360bd6
Georgios Kitsikoudis <gkitsikoudis@gmx.de>
2022-06-02 22:09:12 +0200

    changed main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from workflows.get_pitch_report import get_pitch_report_workflow
+
+
 request_url = (
     'https://pitchai-api-prod.proplayai.com/v1/Pitch/GetPitchReport?value={}='
 )
@@ -8,3 +11,11 @@ headers = {
     'ProplayAIAPIKey': 'e38bbd45-4375-4b20-bda0-bdbc5f3123b2',
     'Path': '/v1/Pitch/GetPitchReport?value={}='
 }
+
+
+print(get_pitch_report_workflow(
+    request_url=request_url,
+    pitch_url='https://pitchai.proplayai.com/shared/report/pitch/NTc1MDM%3D',
+    headers=headers
+    )
+)
