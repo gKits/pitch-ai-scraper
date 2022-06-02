4f46d31a25ee6ccc0a9f81dcc665ec4e2a72dd8d
Georgios Kitsikoudis <gkitsikoudis@gmx.de>
2022-06-02 21:47:15 +0200

    updatet structure

---
+++ main.py
@@ -0,0 +1,10 @@
+request_url = (
+    'https://pitchai-api-prod.proplayai.com/v1/Pitch/GetPitchReport?value={}='
+)
+
+headers = {
+    'Authority': 'pitchai-api-prod.proplayai.com',
+    'Referer': 'https://pitchai.proplayai.com/',
+    'ProplayAIAPIKey': 'e38bbd45-4375-4b20-bda0-bdbc5f3123b2',
+    'Path': '/v1/Pitch/GetPitchReport?value={}='
+}
