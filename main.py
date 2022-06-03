0954169a944f986401fdde7a4e435f5581d30d55
Georgios Kitsikoudis <56302678+gKits@users.noreply.github.com>
2022-06-03 08:29:15 +0000

    Updated main

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from workflows.get_pitch_report import get_pitch_report_workflow
+from workflows.get_kinematics_data import get_kinamtics_data_workflow
 
 
 request_url = (
-    'https://pitchai-api-prod.proplayai.com/v1/Pitch/GetPitchReport?value={}='
+    'https://pitchai-api-prod.proplayai.com/v1/Pitch/GetUnsubscribedUserPitchReport?value={}='
 )
 
 headers = {
     'Authority': 'pitchai-api-prod.proplayai.com',
     'Referer': 'https://pitchai.proplayai.com/',
     'ProplayAIAPIKey': 'e38bbd45-4375-4b20-bda0-bdbc5f3123b2',
-    'Path': '/v1/Pitch/GetPitchReport?value={}='
+    'Path': '/v1/Pitch/GetUnsubscribedUserPitchReport?value={}='
 }
 
-
-print(get_pitch_report_workflow(
+pitch_report = get_pitch_report_workflow(
     request_url=request_url,
-    pitch_url='https://pitchai.proplayai.com/shared/report/pitch/NTc1MDM%3D',
+    pitch_url='https://pitchai.proplayai.com/shared/report/pitch/NTc1MDM%3D',       
     headers=headers
-    )
 )
+
+if pitch_report:
+    pass
